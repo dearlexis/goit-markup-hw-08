e28752773b9d1abc3359ee52f9f0f5f1ca6a5e96
dearlexis <104580408+dearlexis@users.noreply.github.com>
2022-07-12 12:45:08 +0300

    adds bug fixes

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,14 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
+    refs.body.classList.toggle('no-scroll');
     refs.menu.classList.toggle('is-hidden');
   }
 })();
--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,14 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    refs.body.classList.toggle('no-scroll');
     refs.modal.classList.toggle('is-hidden');
   }
 })();
